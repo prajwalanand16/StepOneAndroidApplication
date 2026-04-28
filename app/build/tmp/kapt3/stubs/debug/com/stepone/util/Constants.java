244cf95c0f92d0249189511df4e756ccdf634bd5
Prajwal Anand <prajwalanand16@gmail.com>
2026-04-28 15:13:52 +0530

    Prepare build.gradle for release with minification and update AdMob App ID placeholder

--- app/build/tmp/kapt3/stubs/debug/com/stepone/util/Constants.java
+++ app/build/tmp/kapt3/stubs/debug/com/stepone/util/Constants.java
@@ -1,11 +1,10 @@
 package com.stepone.util;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000&\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0000\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\b\n\u0002\b\u0005\b\u00c6\u0002\u0018\u00002\u00020\u0001:\u0001\u000fB\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u000e\u0010\r\u001a\u00020\u00072\u0006\u0010\u000e\u001a\u00020\u000bR\u000e\u0010\u0003\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000R\u0017\u0010\u0005\u001a\b\u0012\u0004\u0012\u00020\u00070\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\b\u0010\tR\u000e\u0010\n\u001a\u00020\u000bX\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\f\u001a\u00020\u000bX\u0086T\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0010"}, d2 = {"Lcom/stepone/util/Constants;", "", "()V", "CHANNEL_ID", "", "MILESTONES", "", "Lcom/stepone/util/Constants$Milestone;", "getMILESTONES", "()Ljava/util/List;", "NOTIFICATION_ID", "", "STEP_GOAL", "getMilestoneForSteps", "steps", "Milestone", "app_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000&\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0000\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\b\n\u0002\b\u0004\b\u00c6\u0002\u0018\u00002\u00020\u0001:\u0001\u000eB\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u000e\u0010\f\u001a\u00020\u00072\u0006\u0010\r\u001a\u00020\u000bR\u000e\u0010\u0003\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000R\u0017\u0010\u0005\u001a\b\u0012\u0004\u0012\u00020\u00070\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\b\u0010\tR\u000e\u0010\n\u001a\u00020\u000bX\u0086T\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u000f"}, d2 = {"Lcom/stepone/util/Constants;", "", "()V", "CHANNEL_ID", "", "MILESTONES", "", "Lcom/stepone/util/Constants$Milestone;", "getMILESTONES", "()Ljava/util/List;", "NOTIFICATION_ID", "", "getMilestoneForSteps", "steps", "Milestone", "app_debug"})
 public final class Constants {
     public static final int NOTIFICATION_ID = 1;
     @org.jetbrains.annotations.NotNull()
     public static final java.lang.String CHANNEL_ID = "step_counter_channel";
-    public static final int STEP_GOAL = 10000;
     @org.jetbrains.annotations.NotNull()
     private static final java.util.List<com.stepone.util.Constants.Milestone> MILESTONES = null;
     @org.jetbrains.annotations.NotNull()
